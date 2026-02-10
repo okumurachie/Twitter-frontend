681923c50ef99c1c5784f7d00b80a10a3f8f23b1
okumura chie <neco9145@me.com>
2026-02-10 12:22:17 +0900

    Firrebaseセットアップ

---
+++ plugins/firebase.client.js
@@ -0,0 +1,24 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+export default defineNuxtPlugin(() => {
+    const firebaseConfig = {
+        apiKey: config.public.firebaseApiKey,
+        authDomain: config.public.firebaseAuthDomain,
+        projectId: config.public.firebaseProjectId,
+        storageBucket: config.public.firebaseStorageBucket,
+        messagingSenderId: config.public.firebaseMessagingSenderId,
+        appId: config.public.firebaseAppId,
+    };
+
+    const app = initializeApp(firebaseConfig);
+
+    const auth = getAuth(app);
+
+    return {
+        provide: {
+            firebase: app,
+            auth: auth,
+        },
+    };
+});
